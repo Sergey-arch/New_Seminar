4038b5b19cc01c26a4fe6fc39d8877a32d281c2a
sergey <mainsackliem@mail.ru>
2022-04-20 01:01:38 +0500

    num^2

---
+++ task_000/Program.cs
@@ -0,0 +1,6 @@
+﻿// Вывести квадрат числа
+
+int num = new Random().Next(1, 10); // Случайное число от 1 до 9(1, 10-1)
+Console.WriteLine("num = " + num);
+
+Console.WriteLine(num + " ^ 2 = " + num * num);
