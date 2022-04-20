273e94b60a397ae554edc2bdce1f87432d9d2612
sergey <mainsackliem@mail.ru>
2022-04-20 23:38:25 +0500

    num^2

---
+++ task_000b/Program.cs
@@ -0,0 +1,8 @@
+﻿// Вывести квадрат числа (Print the square of the number)
+
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine()); // Запрос цифры на ввод и
+                                               // конвертировать строку в цифру
+int square = num * num;
+
+Console.WriteLine("Ответ: " + square);
