e2d7f71eac33b23cfdc9b3ae8fd5adf094a9fbd5
sergey <mainsackliem@mail.ru>
2022-04-24 11:49:29 +0500

    a=b^2

---
+++ task_001a/Program.cs
@@ -0,0 +1,18 @@
+﻿// По двум заданным числам проверять является ли первое квадратом второго
+
+Console.Write("Введите число numberA: ");
+int numberA = Convert.ToInt32(Console.ReadLine()); // Запрос цифры на ввод и
+                                               // конвертировать строку в цифру
+
+Console.Write("Введите число numberB: ");
+int numberB = Convert.ToInt32(Console.ReadLine()); // Запрос цифры на ввод и
+                                               // конвертировать строку в цифру
+
+if (numberA == numberB * numberB)
+{
+    Console.WriteLine("numberA   Является квадратом numberB");
+}
+else
+{
+    Console.WriteLine("numberA   Не является квадратом numberB");
+}
