b23f1f4d549a86db5e42e83829309543c707b324
sergey <mainsackliem@mail.ru>
2022-04-24 11:57:30 +0500

    a=b^2

---
+++ task_001b/Program.cs
@@ -0,0 +1,10 @@
+﻿// По двум заданным числам проверять является ли первое квадратом второго
+
+int numberA = new Random().Next(1, 16); // Случайное число от 1 до 100(1, 101-1)
+Console.WriteLine("numberA = " + numberA);
+
+int numberB = new Random().Next(1, 5); // Случайное число от 1 до 10(1, 11-1)
+Console.WriteLine("numberB = " + numberB);
+
+if (numberA == numberB * numberB) Console.WriteLine("numberA   Является квадратом numberB");
+else Console.WriteLine("numberA   Не является квадратом numberB");
