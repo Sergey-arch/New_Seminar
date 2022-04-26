12307ea2f73617022b6828e13dbd1502a15b5adc
sergey <mainsackliem@mail.ru>
2022-04-26 22:42:15 +0500

    from -n to n

---
+++ task_005/Program.cs
@@ -0,0 +1,12 @@
+﻿// Показать числа от -N до N
+
+int numberA = new Random().Next(1, 10); // 1, 2, ... 9
+Console.WriteLine("numberA = " + numberA);
+
+int i = numberA * -1;
+
+while(i != numberA + 1)
+{
+    Console.WriteLine("n = " + i);
+    i++;
+}
