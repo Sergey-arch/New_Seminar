59d6a592cfb39739892637c54dece161a0b68bed
sergey <mainsackliem@mail.ru>
2022-04-26 23:35:15 +0500

    from -n to n

---
+++ task_005a/Program.cs
@@ -0,0 +1,20 @@
+﻿// Показать числа от -N до N
+
+Console.Write("Введите число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+int i = numberA * -1;
+
+while(i <= numberA)
+{
+    Console.Write(i);
+    i++;
+    if(i <= numberA)
+    {
+        Console.Write(", ");
+    }
+}
+
+
+
+
