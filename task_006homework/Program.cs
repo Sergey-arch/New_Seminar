ae6bb0f329263837c536bfc073e0ba495129dcaa
sergey <mainsackliem@mail.ru>
2022-04-28 23:28:09 +0500

    even number

---
+++ task_006homework/Program.cs
@@ -0,0 +1,13 @@
+﻿// Чётное число (even number)
+
+int numberA = new Random().Next(1, 1000); // 1, 2, ... 999
+Console.WriteLine("numberA = " + numberA);
+
+if (numberA % 2 == 0)
+{
+    Console.WriteLine("число numberA = " + numberA + " - Является четным");
+}
+else
+{
+    Console.WriteLine("число numberA = " + numberA + " - Не является четным");
+}
