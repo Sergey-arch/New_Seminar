ddcbfc30023c15229091467421ccddb4d5cb38b9
sergey <mainsackliem@mail.ru>
2022-04-28 22:55:13 +0500

    max min 2 nambers

---
+++ task_002homework/Program.cs
@@ -0,0 +1,18 @@
+﻿// Максимальное и минимальное из двух чисел
+
+int numberA = new Random().Next(1, 11); // 1 2 3 4 ... 10
+Console.WriteLine("numberA  " + numberA);
+
+int numberB = new Random().Next(1, 11); // 1 2 3 4 ... 10
+Console.WriteLine("numberB  " + numberB);
+
+if (numberA > numberB)
+{
+    Console.WriteLine("numberA   Является наибольшим числом " + numberA);
+    Console.WriteLine("numberB   Является наименьшим числом " + numberB);
+}
+else
+{
+    Console.WriteLine("numberA   Является наименьшим числом " + numberA);
+    Console.WriteLine("numberB   Является наибольшим числом " + numberB);
+}
