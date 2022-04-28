f3505aae9eb712cdf5fef68a3342bffd6e163b95
sergey <mainsackliem@mail.ru>
2022-04-28 23:40:59 +0500

    even number  0 to N

---
+++ task_008homework/Program.cs
@@ -0,0 +1,12 @@
+﻿// Четные числа от 0 до N (even number  0 to N)
+
+int numberA = new Random().Next(10, 20); // 10, 11, ... 19
+Console.WriteLine("numberA = " + numberA);
+
+for (int i = 0; i <= numberA; i++)
+{
+    if ((i % 2 == 0) && !(i == 1))
+    {
+        Console.Write(i + ", ");
+    }
+}
