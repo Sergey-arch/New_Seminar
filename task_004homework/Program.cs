0cb6cc513735ca8b1336aeb5e0c4455dad615fb0
sergey <mainsackliem@mail.ru>
2022-04-28 23:16:53 +0500

    max 3 nambers

---
+++ task_004homework/Program.cs
@@ -0,0 +1,18 @@
+﻿// Максимальное из трех чисел
+
+int numberA = new Random().Next(1, 11); // 1 2 3 4 ... 10
+Console.WriteLine("numberA  " + numberA);
+
+int numberB = new Random().Next(1, 11); // 1 2 3 4 ... 10
+Console.WriteLine("numberB  " + numberB);
+
+int numberС = new Random().Next(1, 11); // 1 2 3 4 ... 10
+Console.WriteLine("numberС  " + numberС);
+
+int max = numberA;
+
+if(numberA > max) max = numberA;
+if(numberB > max) max = numberB;
+if(numberС > max) max = numberС;
+
+Console.WriteLine("max =  " + max);
