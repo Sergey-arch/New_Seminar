5497b4f2cec1389cc6ff003b105020119664a17e
sergey <mainsackliem@mail.ru>
2022-05-02 21:52:47 +0500

    sum

---
+++ a/Program.cs
@@ -0,0 +1,14 @@
+﻿
+int sum = Sum(15, 62);
+Print(Convert.ToString(sum));
+
+int Sum(int numberOne, int numberTwo)
+{
+    int sum = numberOne + numberTwo;
+    return sum;
+}
+
+void Print(string argument)
+{
+    Console.WriteLine(argument);
+}
