7c6ce7a984cf282170e13751957a7caa66e2ffb7
sergey <mainsackliem@mail.ru>
2022-05-02 22:37:58 +0500

    array Random numbers

---
+++ b/Program.cs
@@ -0,0 +1,15 @@
+﻿// Заполнить массив рандомными числами
+
+int[] numbers = new int[5];
+
+FillIntArray(numbers);
+
+void FillIntArray(int[] array)
+{
+    int index = 0;
+    while (index < numbers.Length)
+    {
+        numbers[index] = new Random().Next(0, 100);
+        index++;
+    }
+}
