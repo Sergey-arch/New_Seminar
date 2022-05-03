991f9c77a278cdd0ad4310fdda3b81bd908309a2
sergey <mainsackliem@mail.ru>
2022-05-03 22:37:57 +0500

    456 ->46

---
+++ task_011/Program.cs
@@ -0,0 +1,12 @@
+﻿// Из рандомного трехзначного числа удалить вторую цифру
+
+int numberA = new Random().Next(100, 1000);
+Console.WriteLine("numberA = " + numberA);
+
+int n1 = numberA /100 % 10;
+int n2 = numberA /10 % 10;
+int n3 = numberA % 10;
+
+int numberB = n1* 10 + n3;
+
+Console.WriteLine("Удаленная втора цифра 3-х значного числа numberA = " + numberB);
