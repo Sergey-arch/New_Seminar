a8c8eab789cfa85f30f4706c12a4eb7581662984
sergey <mainsackliem@mail.ru>
2022-05-04 23:31:42 +0500

    456 ->46

--- task_011a/Program.cs
+++ task_011a/Program.cs
@@ -16,18 +16,5 @@ int n3 = numberA % 10;
 
 int numberB = n1* 10 + n3;
 
-Console.WriteLine("Удаленная втора цифра 3-х значного числа numberA = " + numberB);
-
-
-/*
-int numberA = new Random().Next(100, 1000);
-Console.WriteLine("numberA = " + numberA);
-
-int n1 = numberA /100 % 10;
-int n2 = numberA /10 % 10;
-int n3 = numberA % 10;
-
-int numberB = n1* 10 + n3;
-
-Console.WriteLine("Удаленная втора цифра 3-х значного числа numberA = " + numberB);
-*/
+Console.WriteLine("Удаленная вторая цифра 3-х значного числа numberA = " + n2);
+Console.WriteLine("Первая и третья цифра 3-х значного числа numberA = " + numberB);
--- task_011b/Program.cs
+++ task_011b/Program.cs
@@ -1,13 +1,11 @@
-﻿// Из рандомного трехзначного числа удалить вторую цифру
-
-int numberA = new Random().Next(100, 1000);
+﻿int numberA = new Random().Next(100, 1000);
 Console.WriteLine("numberA = " + numberA);
 
-int n1 = numberA /100 % 10;
+int n1 = numberA /100;
 int n2 = numberA /10 % 10;
 int n3 = numberA % 10;
 
-int numberB = n1* 10 + n3;
+string numberB = Convert.ToString(n1) + Convert.ToString(n3);
 
 Console.WriteLine("Удаленная вторая цифра 3-х значного числа numberA = " + n2);
 Console.WriteLine("Первая и третья цифра 3-х значного числа numberA = " + numberB);
